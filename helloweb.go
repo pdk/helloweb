ed59a2af800040deb66ba5d0709e5b57d9c4aca6
Patrick Kelly <patrick.kelly@zuora.com>
2018-12-14 14:30:19 -0800

    doh

---
+++ helloweb.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<p>Hello, web</p>")
+}
